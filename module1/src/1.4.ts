f327f9bf56327b7f62ab5b3c89f9ea67bce88b8e
S M Arifur Rahman <smarifurrahman82@gmail.com>
2024-05-06 01:21:14 +0600

    first commit

---
+++ module1/src/1.4.ts
@@ -0,0 +1,17 @@
+// Basic Data Types
+
+// string
+let firstName: string = 'Arif';
+
+//number
+let roll: number = 123;
+
+//boolean
+let isAdmin: boolean = true;
+
+//undefined
+
+let x: undefined = undefined;
+
+//null]
+let y: null = null;
---
+++ module1/src/test.ts
@@ -0,0 +1,1 @@
+const test: string = "I'm  a test";
