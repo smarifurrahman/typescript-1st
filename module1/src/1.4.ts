e6d3ddb93e673b10ea4be0de4bf2f6142e0ced57
S M Arifur Rahman <smarifurrahman82@gmail.com>
2024-05-06 01:44:10 +0600

    1.4 completed

--- module1/src/1.4.ts
+++ module1/src/1.4.ts
@@ -15,3 +15,34 @@ let x: undefined = undefined;
 
 //null]
 let y: null = null;
+
+// let d;
+let d: number;
+
+// d = 'abc';
+d = 123;
+
+//Array
+let friends: string[] = ['Arif', 'Hasnat'];
+let eligibleRollList: number[] = [19, 43, 44]; 
+
+// eligibleRollList.push('23');
+eligibleRollList.push(23);
+
+//tuple --> array --> order --> type of values
+// let coordinates: [number, number] = [23, 34, 11]
+let coordinates: [number, number] = [23, 34]
+
+let ageName: [number, string] = [12, 'Arif'];
+
+// ageName[0] = 'Mr Y';
+ageName[0] = 34;
+
+let ageNameBool: [number, string, boolean] = [12, 'Arif', true];
+
+
+
+
+
+
+
